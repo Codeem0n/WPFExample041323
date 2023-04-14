1cde3cfca769da2b852e0f84ba6e6b8917b05b60
etehrani <etehrani@DEVICENET>
2023-04-13 23:10:59 -0500

    Add project files.

---
+++ WpfAppExample/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string textBoxText;
+
+        public List<Point> textList = new List<Point>();
+
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxText = MyTextBox.Text;
+
+            MessageBox.Show(textBoxText);
+
+            MyListbox.ItemsSource = textList;
+
+            Combo1.ItemsSource = textList;
+
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point point = new Point();
+            point = e.GetPosition(MyCanvas);
+
+            //MessageBox.Show(point.X.ToString()+ " , " + point.Y.ToString());
+
+            string xyz = point.X.ToString() + " , " + point.Y.ToString();
+
+            textList.Add(point);
+
+
+
+            //MyLabel.Content = xyz;
+
+
+
+        }
+    }
+}
